896e34c6021395980a11b042f455c91e5c1b06be
jackson <computadordb@gmail.com>
2024-08-30 18:33:34 -0300

    add: novo treino de drible/melhorando alguns pontos do css para ser responsivo

--- ballers085/views.py
+++ ballers085/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render
 from .models import Exercicios, Treinos
+from datetime import datetime
+from django.shortcuts import get_object_or_404
 
 def index(request):
     return render(request, 'pages/index.html')
  
 def treinos_categoria(request, categoria):
     try:
-        treinos = Treinos.objects.filter(categoria=categoria)
+        treinos = Treinos.objects.filter(categoria=categoria, data_criacao__lte=datetime.now()).order_by('-data_criacao')
+        if treinos.count() == 0:
+            return render(request, 'pages/404.html')
+
         if categoria =="academia":
             return render(request, 'pages/treinos_academia.html', {'treinos': treinos, 'categoria': categoria})  
         return render(request, 'pages/treinos.html',{'treinos': treinos, 'categoria': categoria})
     except Treinos.DoesNotExist:
-        return render(request,"404.html")
+        return render(request,"pages/404.html")
 
 def psicobaska(request):
     return render(request, 'pages/psicobaska.html', {'extracss':'css/psicobaska.css'})
 
-def treino_exercicio(request,categoria,nome_treino):
-    treino_escolhido = Treinos.objects.filter(caminho_treino_url=nome_treino).first()
+def treino_exercicio(request, categoria, nome_treino):
     try:
+        treino_escolhido = get_object_or_404(Treinos,caminho_treino_url=nome_treino)
         exercicios = Exercicios.objects.filter(treino=treino_escolhido)
+        return render(request, 'pages/exercicios.html', {'exercicios': exercicios, "nome_treino": treino_escolhido.nome})
     except Exercicios.DoesNotExist:
-        return render(request,"404.html")
-    return render(request, 'pages/exercicios.html', {'exercicios': exercicios,"nome_treino":treino_escolhido.nome})
+        return render(request, "404.html")
 
 
